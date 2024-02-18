5b82dfb35dd8440323e960b31c760b88671bf250
Mithzzx <Mithzzx@users.noreply.github.com>
2024-02-18 22:29:19 +0530

    Level 3

--- Assets/Scripts/LimitFPS.cs
+++ Assets/Scripts/LimitFPS.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class LimitFPS : MonoBehaviour
 {
-    [Serializefield] int fps = 60;
+    [SerializeField] int fps = 60;
+
     void Start()
     {
         Application.targetFrameRate = fps;
